92c9d66aff2ab1192c697bb99c98bb36a7867839
Jin Ikeda <jikeda@users.noreply.github.com>
2024-08-14 21:37:07 -0500

    replace by click for submodules

---
+++ src/click_main.py
@@ -0,0 +1,34 @@
+# from src.CRMS_Continuous_Hydrographic2subsets import continuous_subcommand
+# from src.CRMS_Discrete_Hydrographic2subsets import discrete_subcommand
+# from src.CRMS2Resample import resample_subcommand
+# from src.CRMS2Plot import plot_subcommand
+# from src.CRMS_general_functions import *
+from CRMS_Continuous_Hydrographic2subsets import continuous_subcommand
+from CRMS_Discrete_Hydrographic2subsets import discrete_subcommand
+from CRMS2Resample import resample_subcommand
+from CRMS2Plot import plot_subcommand
+from CRMS_general_functions import *
+
+########################################################################################################################
+### Cautions
+########################################################################################################################
+
+"""
+click.option argument (--abcd) must be lowercase
+"""
+
+
+@click.group()
+def click_main():
+    """CRMS2Map Command Line Interface"""
+    pass
+
+
+# Add subcommands to the group
+click_main.add_command(continuous_subcommand, name="continuous")
+click_main.add_command(discrete_subcommand, name="discrete")
+click_main.add_command(resample_subcommand, name="resample")
+click_main.add_command(plot_subcommand, name="plot")
+
+if __name__ == "__main__":
+    click_main()
